6bf05093129fcf11b1db533e5bebc39acf6108c7
Floriansalducci <florian.salducci@gmail.com>
2020-08-28 14:23:31 +0200

    function + template show

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Repository\AdRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,14 +13,29 @@ class AdController extends AbstractController
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index()
+    public function index(AdRepository $repository)
     {
-        $repository = $this->getDoctrine()->getRepository(Ad::class);
 
         $ads = $repository->findAll();
 
         return $this->render('ad/index.html.twig', [
             'ads' => $ads,
         ]);
+    }
+
+    /**
+     *
+     * @Route("/ads/{slug}", name="ads_show")
+     *
+     */
+    public function show($slug, AdRepository $repository){
+
+        // Je récupére l'annonce qui correspond au slug
+        $ad = $repository->findOneBySlug($slug);
+
+        return $this->render('ad/show.html.twig', [
+            'ad'=> $ad
+        ]);
+
     }
 }
