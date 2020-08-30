ec345354956d9dc667487db10753603c5d875d38
Floriansalducci <florian.salducci@gmail.com>
2020-08-30 16:47:01 +0200

    form new_ads

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdController extends AbstractController
@@ -24,18 +26,34 @@ class AdController extends AbstractController
     }
 
     /**
+     * Permet de créer une annonce
+     * Mettre avant la methode show sinon symfo cherche un slug new a la place la route
+     *
+     * @Route("/ads/new", name="ads_create")
+     */
+
+    public function create(){
+        $ad = new Ad();
+
+        $form= $this->createForm(AdType::class, $ad);
+
+        return $this->render('ad/new.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet d'afficher une annonce
      *
      * @Route("/ads/{slug}", name="ads_show")
      *
      */
-    public function show($slug, AdRepository $repository){
-
-        // Je récupére l'annonce qui correspond au slug
-        $ad = $repository->findOneBySlug($slug);
+    public function show(Ad $ad){
 
         return $this->render('ad/show.html.twig', [
             'ad'=> $ad
         ]);
-
     }
+
+
 }
