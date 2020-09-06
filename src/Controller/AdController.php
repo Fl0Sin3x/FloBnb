7007e5d9699a038ba7cbba97267c7eb5b4ff4ec8
Floriansalducci <florian.salducci@gmail.com>
2020-09-06 20:53:41 +0200

    form edit

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -83,5 +83,42 @@ class AdController extends AbstractController
         ]);
     }
 
+    /**
+     * Permet d'afficher le formulaire d'édition
+     *
+     * @Route("/ads/{slug}/edit", name="ads_edit")
+     *
+     */
+    public function edit(Request $request, Ad $ad,EntityManagerInterface $manager ){
+
+        $form= $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            foreach ($ad->getImages() as $image){
+                $image->setAd($ad);
+                $manager->persist($image);
+            }
+
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été modifiée"
+            );
+
+
+            return $this->redirectToRoute('ads_show', [
+                'slug'=> $ad->getSlug()
+            ]);
+        }
+
+        return $this->render('ad/edit.html.twig', [
+            'form'=> $form->createView(),
+            'ad'=> $ad
+        ]);
+    }
 
 }
