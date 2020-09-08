a98586edb2899c9a8a75f42d2797d180be7cb60d
Floriansalducci <florian.salducci@gmail.com>
2020-09-08 05:03:49 +0200

    add profil page++

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -153,4 +153,15 @@ class AccountController extends AbstractController
         ]);
 
     }
+    /**
+     * Permet d'afficher le profil de l'utilisateur connecté
+     *
+     * @Route("/account", name="account_index")
+     */
+    public function myAccount()
+    {
+        return $this->render('user/index.html.twig', [
+            'user' => $this->getUser()
+          ]);
+    }
 }
