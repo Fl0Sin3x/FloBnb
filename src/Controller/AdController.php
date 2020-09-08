e91dbbee4a96c9d17b77cb174d7a114f59d6fdcd
Floriansalducci <florian.salducci@gmail.com>
2020-09-08 23:42:29 +0200

    ads delete

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -127,4 +127,23 @@ class AdController extends AbstractController
         ]);
     }
 
+    /**
+     * Permet de supprimer une annonce
+     *
+     * @Route("/ads/{slug}/delete", name="ads_delete")
+     * @Security("is_granted('ROLE_USER') and user === ad.getAuthor()", message="Cette annonce ne vous appartient pas, vous ne pouvez pas la supprimer")
+     *
+     */
+    public function delete(Ad $ad,EntityManagerInterface $manager){
+        $manager->remove($ad);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            "L'annonce <strong>{$ad->getTitle()}</strong> a bien été supprimer"
+        );
+
+        return $this->redirectToRoute("ads_index");
+    }
+
 }
