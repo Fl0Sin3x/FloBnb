92afd7a4768c969bee1802df8b64bbeed637ca3c
Floriansalducci <florian.salducci@gmail.com>
2020-09-15 04:49:31 +0200

    dashboard stats

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Service\Stats;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +12,19 @@ class AdminDashboardController extends AbstractController
     /**
      * @Route("/admin", name="admin_dashboard")
      */
-    public function index()
+    public function index(EntityManagerInterface $manager, Stats $stats)
     {
+
+
+        $statistiques = $stats->getStats();
+        $bestAds = $stats->getAdsStats('DESC');
+        $worseAds = $stats->getAdsStats('ASC');
+
+
         return $this->render('admin/dashboard/index.html.twig', [
-            'controller_name' => 'AdminDashboardController',
+            'stats'=> $statistiques,
+            'bestAds'=> $bestAds,
+            'worseAds'=> $worseAds
         ]);
     }
 }
